a230d5b4c0c5665594a0005fe85745d743c89ad4
unknown <lmeier815@FHS-LAB127-LEN3.fhs.win.fuhsd.org>
2016-05-12 08:14:25 -0700

    change jar file to make JSON parsing from string available

--- src/JsonTutorial.java
+++ src/JsonTutorial.java
@@ -1,22 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
-import org.json.simple.JSONObject;
+import org.json.JSONObject;
 
 public class JsonTutorial {
+	
 	public static void main(String[] args) {
 		try {
 			URL url = new URL("https://data.sfgov.org/api/views/w969-5mn4/rows.json?accessType=DOWNLOAD");
 			InputStream urlInputStream = url.openConnection().getInputStream();
 			
-			BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8")); 
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(urlInputStream, "UTF-8")); 
 			StringBuilder responseStrBuilder = new StringBuilder();
 
 			String inputStr;
 			while ((inputStr = streamReader.readLine()) != null)
 			    responseStrBuilder.append(inputStr);
 			JSONObject data = new JSONObject(responseStrBuilder.toString());
+			System.out.println(data.toString());
 		} catch (Exception e) {
 			System.out.println(e);
 		}
